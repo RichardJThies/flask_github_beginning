06973d19dd9f4e61c3ac7b6f5f78560cf2a62521
SalvLav <richardjthies95@gmail.com>
2023-10-19 14:48:44 -0500

    finished program

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
+"""Base of the program, sets up the flask web application and the program"""
 from flask import Flask, render_template, request  # NOT the same as requests 
+from github_api import get_github_user# call just the one function needed
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')#/ is the homepage/base directory. Often omitted by browser when on homepage
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html')#route handler function/view functions always need to return something
 
+@app.route('/get_user')#route handler function/view function. A view is a webpage. Dob't forget the / before get_user
+def get_user_info():#flask is unpacking query parameters in url and coverting them into a dictionary
+    """get user info from GitHub api, display on new page"""
+    print(f'form data is {request.args}')#request is a built in variable from flask, not requests libray. Stores data about the request made by the browser to the server
+    #every request made to server, server should always respond. No matter how bad/crash causing/weird/unexpected, the server should respond
+    username = request.args.get('username')#classic style, won't error if username data is not provided, instead returns None. Use request.args.get() to read data for each input/field if needed
+    # username = request.args('username')#more common style, but does error if no username data is provided
+    user_info, error_message = get_github_user(username)#user_info will be dictionary because json data. Request the data from
+    if error_message:#sending the error string returned from github_api to new page
+        return render_template('error.html', error=error_message)
+    else:
+        return render_template('github.html',  user_info=user_info)#adding keyword arguments to template. "github_username" is the label/name of the data 
 
 if __name__ == '__main__':
     app.run()
--- github_api.py
+++ github_api.py
@@ -1,1 +1,34 @@
 # TODO make requests to github API
+import requests
+import logging
+
+def get_github_user(username):
+    #When dealing with a function that needs to send data a/ indicate errs, return a tuple of (data and err msg)
+
+    #if things work, return (data, None)
+
+    #if things don't work, there's an err,so return (None. error value/msg)
+
+    try:#example err handling. more things could go wrong
+        response = requests.get(f'https://api.github.com/users/{username}')
+        #404 - username not found
+        if response.status_code == 404: #not found
+            return None, f'Username {username} not found'#tuple of data, or in this case (None, and an err string)
+        #other error
+        response.raise_for_status()#500 err from github, sends to except handler 
+        #success/happy path
+        response_json = response.json()#need to turn response object into reponse.json object
+        user_info = extract_user_info(response_json)
+        return user_info, None #return a tuple of (data and err msg, or in this case None)
+    except Exception as e:
+        logging.exception(e)#deals with a general problem, like connection error
+        return None, e, 'Error, connecting to GitHub'#if things don't work, there's an error, return (None. error msg)
+
+def extract_user_info(json_response):#pulling out wanted properties
+    return {
+        'login': json_response.get('login'),
+        'name': json_response.get('name'),
+        'avatar_url': json_response.get('avatar_url'),
+        'home_page': json_response.get('html_url'),
+        'public_repos': json_response.get('public_repos')
+    }#string before the : are user created names, string in the () need to match the properties in the json response
